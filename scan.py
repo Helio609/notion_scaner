95d13c14295e70d5b19010c757c316c636167c98
Helio <helio609.dev@outlook.com>
2024-05-27 22:56:50 +0800

    refactor

--- scan.py
+++ scan.py
@@ -1,83 +1,154 @@
-import os
+from notion_client import Client as NotionClient
+from supabase import create_client, Client as Supabase
+from os import getenv
+from dotenv import load_dotenv
+from enum import Enum
 from queue import Queue
 
-from dotenv import load_dotenv
-from notion_client import Client as NotionClient
-from supabase import Client as SupabaseClient
-from supabase import create_client
+
+class DetectedType(Enum):
+    DATABASE = 1
+    PAGE = 2
+    NONE = 3
 
 
-def count(notion_client, root_block: str):
-    block_cnt = 0
-    word_cnt = 0
-    blocks = Queue()
+class Scaner:
+    def __init__(self, auth: str, supabase_url: str, supabase_key: str) -> None:
+        self.__notion_client: NotionClient = NotionClient(auth=auth)
+        self.__supabase: Supabase = create_client(supabase_url, supabase_key)
 
-    root = notion_client.blocks.children.list(block_id=root_block)["results"]
-    for block in root:
-        blocks.put(block)
+        self.__plans = (
+            self.__supabase.table("plans").select("id, root_block").execute().data
+        )
 
-    while not blocks.empty():
-        block = blocks.get()
-        if block["has_children"]:
-            # print(f'BlockId({block["type"]}): {block["id"]} has children')
-            children = notion_client.blocks.children.list(block_id=block["id"])[
-                "results"
-            ]
-            for child in children:
-                # print(f'\tBlockId({block["type"]}): {child["id"]}')
-                blocks.put(child)
+    def __insert(self, plan_id: str, block_cnt: int, word_cnt: int):
+        self.__supabase.table("statistics").insert(
+            {
+                "plan_id": plan_id,
+                "block_cnt": block_cnt,
+                "word_cnt": word_cnt,
+            }
+        ).execute()
 
-        if "rich_text" in block[block["type"]]:
-            # print(f'BlockId({block["type"]}): {block["id"]} has rich text')
-            word_cnt += sum(
-                [
-                    len(rich_text["plain_text"])
-                    for rich_text in block[block["type"]]["rich_text"]
-                ]
-            )
+    def __dectect(self, id: str):
+        block = self.__notion_client.blocks.retrieve(id)
 
         if block["type"] == "child_database":
-            # print(f'BlockId({block["type"]}): {block["id"]} is a child database')
-            child_database = notion_client.databases.query(database_id=block["id"])[
-                "results"
-            ]
-            for page in child_database:
-                root = notion_client.blocks.children.list(block_id=page["id"])[
-                    "results"
-                ]
-                for child_block in root:
-                    blocks.put(child_block)
+            return DetectedType.DATABASE
+        elif block["type"] == "child_page":
+            return DetectedType.PAGE
 
-        block_cnt += 1
-    return block_cnt, word_cnt
+        return None
 
+    def __process_database(self, database_id: str):
+        block_cnt = 0
+        word_cnt = 0
+        has_more = True
+        next_cursor = None
+        while has_more:
+            database = self.__notion_client.databases.query(
+                database_id, next_cursor=next_cursor, page_size=100
+            )
+            if database["has_more"]:
+                next_cursor = database["next_cursor"]
+            else:
+                has_more = False
+            for page in database["results"]:
+                b_cnt, w_cnt = self.__process_page(page["id"])
+                block_cnt += b_cnt
+                word_cnt += w_cnt
+        return block_cnt, word_cnt
 
-def insert(supabase_client: SupabaseClient, plan_id, block_cnt, word_cnt):
-    supabase_client.table("statistics").insert(
-        {
-            "plan_id": plan_id,
-            "block_cnt": block_cnt,
-            "word_cnt": word_cnt,
-        }
-    ).execute()
+    def __process_page(self, block_id: str):
+        block_cnt = 0
+        word_cnt = 0
+        q = Queue()
+        has_more = True
+        next_cursor = None
 
+        while has_more:
+            blocks = self.__notion_client.blocks.children.list(
+                block_id=block_id, next_cursor=next_cursor, page_size=100
+            )
+            for block in blocks["results"]:
+                q.put(block)
+            if blocks["has_more"]:
+                next_cursor = blocks["next_cursor"]
+            else:
+                has_more = False
 
-def main():
-    load_dotenv()
+        while not q.empty():
+            block = q.get()
 
-    supabase_client: SupabaseClient = create_client(
-        supabase_url=os.getenv("SUPABASE_URL"),
-        supabase_key=os.getenv("SUPABASE_KEY"),
-    )
-    notion_client = NotionClient(auth=os.getenv("NOTION_AUTH"))
+            if block["has_children"]:
+                print(f'BlockId({block["type"]}): {block["id"]} has children')
+                has_more = True
+                next_cursor = None
+                while has_more:
+                    children = self.__notion_client.blocks.children.list(
+                        block_id=block["id"],
+                        start_cursor=next_cursor,
+                        page_size=100,
+                    )
+                    for child in children["results"]:
+                        q.put(child)
+                    if children["has_more"]:
+                        next_cursor = children["next_cursor"]
+                    else:
+                        has_more = False
 
-    plans = (supabase_client.table("plans").select("id, root_block").execute()).data
+            if "rich_text" in block[block["type"]]:
+                print(f'BlockId({block["type"]}): {block["id"]} has rich text')
+                word_cnt += sum(
+                    [
+                        len(rich_text["plain_text"])
+                        for rich_text in block[block["type"]]["rich_text"]
+                    ]
+                )
 
-    for plan in plans:
-        block_cnt, word_cnt = count(notion_client, plan["root_block"])
-        insert(supabase_client, plan["id"], block_cnt, word_cnt)
-        print(f"Plan {plan['id']} done. {block_cnt} blocks and {word_cnt} words")
+            if block["type"] == "child_database":
+                print(f'BlockId({block["type"]}): {block["id"]} is a child database')
+                b_cnt, w_cnt = self.__process_database(block["id"])
+                block_cnt += b_cnt
+                word_cnt += w_cnt
+                continue
+
+            block_cnt += 1
+
+        return block_cnt, word_cnt
+
+    def __process(self, id: str):
+        block_cnt = 0
+        word_cnt = 0
+        detectedType = self.__dectect(id)
+        if detectedType == DetectedType.DATABASE:
+            block_cnt, word_cnt = self.__process_database(database_id=id)
+        elif detectedType == DetectedType.PAGE:
+            block_cnt, word_cnt = self.__process_page(block_id=id)
+        else:
+            print("Nothing to do.")
+        return block_cnt, word_cnt
+
+    def __run(self, id: str):
+        return self.__process(id)
+
+    def run(self):
+        for plan in self.__plans:
+            plan_id, id = plan["id"], plan["root_block"]
+            block_cnt, word_cnt = self.__run(id)
+            self.__insert(plan_id, block_cnt, word_cnt)
+            print(f"Plan {plan_id} done, {block_cnt} blocks and {word_cnt} words.")
 
 
 if __name__ == "__main__":
-    main()
+    load_dotenv()
+
+    NOTION_AUTH = getenv("NOTION_AUTH")
+    SUPABASE_URL = getenv("SUPABASE_URL")
+    SUPABASE_KEY = getenv("SUPABASE_KEY")
+
+    if not NOTION_AUTH or not SUPABASE_URL or not SUPABASE_KEY:
+        raise RuntimeError("Please provide env vars")
+
+    scaner = Scaner(NOTION_AUTH, SUPABASE_URL, SUPABASE_KEY)
+    scaner.run()
